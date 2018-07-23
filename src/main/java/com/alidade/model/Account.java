a8f4ffddf3adf4675600b557146b905c3e47bc06
Eric Foster <ericf@zipcoders-MacBook-Pro-3.local>
2018-07-23 14:39:49 -0400

    asdf

--- src/main/java/com/alidade/model/Account.java
+++ src/main/java/com/alidade/model/Account.java
@@ -1,5 +1,8 @@
 package com.alidade.model;
 
+import sun.java2d.cmm.Profile;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,6 +13,7 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    private Profile profile;
+    private Auth auth;
 
 }
